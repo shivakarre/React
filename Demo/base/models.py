344f11228d0270beef92ecff050f8d27e032194d
shivakarre <128774378+shivakarre@users.noreply.github.com>
2023-03-24 10:06:42 +0530

    initial

---
+++ Demo/base/models.py
@@ -0,0 +1,8 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True) 
